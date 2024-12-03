2c36d27a7afa1fd89cedb92b1ef87ce9344c8663
mera22qs <118453987+mera22qs@users.noreply.github.com>
2024-12-03 13:18:23 +0100

    js

---
+++ main.js
@@ -0,0 +1,16 @@
+const greenBtn = document.getElementById("btn-green");
+const pinkBtn = document.getElementById("btn-pink");
+const blueBtn = document.getElementById("btn-blue");
+const bodyElement = document.querySelector("body");
+
+greenBtn.addEventListener("click", function (event) {
+  bodyElement.style.backgroundColor = "#d2e8d7";
+});
+
+pinkBtn.addEventListener("click", function (event) {
+  bodyElement.style.backgroundColor = "#f4d7ec";
+});
+
+blueBtn.addEventListener("click", function (event) {
+  bodyElement.style.backgroundColor = "#dde3e3;";
+});
