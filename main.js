5510fd26158aa25250cbfe06e0fbec267a333e33
mera22qs <118453987+mera22qs@users.noreply.github.com>
2024-12-03 15:47:51 +0100

    dubble click function

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ greenBtn.addEventListener("click", function (event) {
   bodyElement.style.backgroundColor = "#d2e8d7";
 });
 
+greenBtn.addEventListener("dblclick", function (event2) {
+  bodyElement.style.backgroundColor = "#2a7040";
+  window.alert("Surprise");
+});
+
 pinkBtn.addEventListener("click", function (event) {
   bodyElement.style.backgroundColor = "#f4d7ec";
 });
